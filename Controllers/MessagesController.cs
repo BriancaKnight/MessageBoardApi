eae4ce8e23ffb4a35a653b2f80d0fae168390faa
Brianca Knight <briancaknight@gmail.com>
2024-02-20 15:10:30 -0800

    add create and update functionality to messages

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -35,5 +35,63 @@ public async Task<ActionResult<Message>> GetMessage(int messageId)
   return message;
 }
 
+[HttpPost]
+public async Task<ActionResult<Message>> Post(Message message)
+{
+  _db.Messages.Add(message);
+  await _db.SaveChangesAsync();
+  return CreatedAtAction(nameof(GetMessage), new { id = message.MessageId }, message);
+}
+
+
+[HttpPut("{id}")]
+public async Task<IActionResult> Put(int id, Message message)
+{
+  if (id != message.MessageId)
+  { 
+    return BadRequest();
+  }
+
+  _db.Messages.Update(message);
+
+  try
+  {
+    await _db.SaveChangesAsync();
+  }
+  catch(DbUpdateConcurrencyException)
+  {
+    if (!MessageExists(id))
+    {
+      return NotFound();
+    }
+    else
+    {
+      throw;
+    }
+  }
+  return NoContent();
+
+  
+}
+private bool MessageExists(int id)
+{
+  return _db.Messages.Any(e => e.MessageId == id);
+}
+
+// [HttpDelete("{id}")]
+//     public async Task<IActionResult> DeleteMessage(int id)
+//     {
+//       Message message = await _db.Messages.FindAsync(id);
+//       if (message == null)
+//       {
+//         return NotFound();
+//       }
+
+//       _db.Messages.Remove(message);
+//       await _db.SaveChangesAsync();
+
+//       return NoContent();
+//     }
+
 }
 }
